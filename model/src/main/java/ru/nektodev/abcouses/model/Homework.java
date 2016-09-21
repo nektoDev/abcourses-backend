72554130ca7786f813285451bbd50e7ea73bfef7
nektodev <ts.slawa@gmail.com>
2016-09-21 23:50:57 +0300

    Add accessors, eq and hc, toString to Homework POJOs

--- model/src/main/java/ru/nektodev/abcouses/model/Homework.java
+++ model/src/main/java/ru/nektodev/abcouses/model/Homework.java
@@ -1,5 +1,6 @@
 package ru.nektodev.abcouses.model;
 
+import com.google.common.base.Objects;
 import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
@@ -18,4 +19,83 @@ public class Homework implements Serializable{
     private String fileHash;
     private List<HomeworkWord> vocabulary;
     private List<HomeworkWord> pronunciation;
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                .add("id", id)
+                .add("studentId", studentId)
+                .add("date", date)
+                .add("fileHash", fileHash)
+                .add("vocabulary", vocabulary)
+                .add("pronunciation", pronunciation)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Homework homework = (Homework) o;
+        return Objects.equal(id, homework.id) &&
+                Objects.equal(studentId, homework.studentId) &&
+                Objects.equal(date, homework.date) &&
+                Objects.equal(fileHash, homework.fileHash) &&
+                Objects.equal(vocabulary, homework.vocabulary) &&
+                Objects.equal(pronunciation, homework.pronunciation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id, studentId, date, fileHash, vocabulary, pronunciation);
+    }
+
+    public String getId() {
+
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getFileHash() {
+        return fileHash;
+    }
+
+    public void setFileHash(String fileHash) {
+        this.fileHash = fileHash;
+    }
+
+    public List<HomeworkWord> getVocabulary() {
+        return vocabulary;
+    }
+
+    public void setVocabulary(List<HomeworkWord> vocabulary) {
+        this.vocabulary = vocabulary;
+    }
+
+    public List<HomeworkWord> getPronunciation() {
+        return pronunciation;
+    }
+
+    public void setPronunciation(List<HomeworkWord> pronunciation) {
+        this.pronunciation = pronunciation;
+    }
 }
--- model/src/main/java/ru/nektodev/abcouses/model/HomeworkWord.java
+++ model/src/main/java/ru/nektodev/abcouses/model/HomeworkWord.java
@@ -1,5 +1,6 @@
 package ru.nektodev.abcouses.model;
 
+import com.google.common.base.Objects;
 import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
@@ -14,4 +15,64 @@ public class HomeworkWord implements Serializable {
     private String question;
     private String answer;
     private String wordId;
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                .add("id", id)
+                .add("question", question)
+                .add("answer", answer)
+                .add("wordId", wordId)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HomeworkWord that = (HomeworkWord) o;
+        return Objects.equal(id, that.id) &&
+                Objects.equal(question, that.question) &&
+                Objects.equal(answer, that.answer) &&
+                Objects.equal(wordId, that.wordId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id, question, answer, wordId);
+    }
+
+    public String getQuestion() {
+
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getWordId() {
+        return wordId;
+    }
+
+    public void setWordId(String wordId) {
+        this.wordId = wordId;
+    }
+
+    public Integer getId() {
+
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
